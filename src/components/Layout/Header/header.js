df7f50ac03361280efd95ad1479e53385157d829
Kyle Shores <kyle.shores44@gmail.com>
2023-02-07 22:09:02 -0600

    adding another example

--- src/components/Layout/Header/header.js
+++ src/components/Layout/Header/header.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StaticImage } from "gatsby-plugin-image"
 
+import icon from "../../../images/icon.png"
+
 const Header = () => {
   return (
     <>
     <h1>hi</h1>
+    <p>Using static image</p>
     <StaticImage src="../../../images/icon.png"></StaticImage>
+    <p>Using `img`</p>
+    <img src={icon} alt="#" />
+    <p>End of header</p>
+    <hr/>
     </>
   )
 }
